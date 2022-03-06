8a5b881a0b6a19864edd477b2c6bca6b54ff518d
AngelyTC <angelyth312@mesoamericana.edu.gt>
2022-03-05 22:33:49 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿
+namespace LaboratorioNo4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.navegadorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.inicioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.haciaAdelanteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.haciaAtrásToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.historialToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.másVisitadasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.másRecientesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cmbBuscar = new System.Windows.Forms.ComboBox();
+            this.btnIr = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.navegadorToolStripMenuItem,
+            this.historialToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1382, 33);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // navegadorToolStripMenuItem
+            // 
+            this.navegadorToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.inicioToolStripMenuItem,
+            this.haciaAdelanteToolStripMenuItem,
+            this.haciaAtrásToolStripMenuItem,
+            this.salirToolStripMenuItem});
+            this.navegadorToolStripMenuItem.Name = "navegadorToolStripMenuItem";
+            this.navegadorToolStripMenuItem.Size = new System.Drawing.Size(116, 29);
+            this.navegadorToolStripMenuItem.Text = "Navegador";
+            // 
+            // inicioToolStripMenuItem
+            // 
+            this.inicioToolStripMenuItem.Name = "inicioToolStripMenuItem";
+            this.inicioToolStripMenuItem.Size = new System.Drawing.Size(232, 34);
+            this.inicioToolStripMenuItem.Text = "Inicio";
+            this.inicioToolStripMenuItem.Click += new System.EventHandler(this.inicioToolStripMenuItem_Click);
+            // 
+            // haciaAdelanteToolStripMenuItem
+            // 
+            this.haciaAdelanteToolStripMenuItem.Name = "haciaAdelanteToolStripMenuItem";
+            this.haciaAdelanteToolStripMenuItem.Size = new System.Drawing.Size(232, 34);
+            this.haciaAdelanteToolStripMenuItem.Text = "Hacia Adelante";
+            this.haciaAdelanteToolStripMenuItem.Click += new System.EventHandler(this.haciaAdelanteToolStripMenuItem_Click);
+            // 
+            // haciaAtrásToolStripMenuItem
+            // 
+            this.haciaAtrásToolStripMenuItem.Name = "haciaAtrásToolStripMenuItem";
+            this.haciaAtrásToolStripMenuItem.Size = new System.Drawing.Size(232, 34);
+            this.haciaAtrásToolStripMenuItem.Text = "Hacia Atrás";
+            this.haciaAtrásToolStripMenuItem.Click += new System.EventHandler(this.haciaAtrásToolStripMenuItem_Click);
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(232, 34);
+            this.salirToolStripMenuItem.Text = "Salir";
+            this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
+            // 
+            // historialToolStripMenuItem
+            // 
+            this.historialToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.másVisitadasToolStripMenuItem,
+            this.másRecientesToolStripMenuItem});
+            this.historialToolStripMenuItem.Name = "historialToolStripMenuItem";
+            this.historialToolStripMenuItem.Size = new System.Drawing.Size(93, 29);
+            this.historialToolStripMenuItem.Text = "Historial";
+            // 
+            // másVisitadasToolStripMenuItem
+            // 
+            this.másVisitadasToolStripMenuItem.Name = "másVisitadasToolStripMenuItem";
+            this.másVisitadasToolStripMenuItem.Size = new System.Drawing.Size(225, 34);
+            this.másVisitadasToolStripMenuItem.Text = "Más Visitadas";
+            this.másVisitadasToolStripMenuItem.Click += new System.EventHandler(this.másVisitadasToolStripMenuItem_Click);
+            // 
+            // másRecientesToolStripMenuItem
+            // 
+            this.másRecientesToolStripMenuItem.Name = "másRecientesToolStripMenuItem";
+            this.másRecientesToolStripMenuItem.Size = new System.Drawing.Size(225, 34);
+            this.másRecientesToolStripMenuItem.Text = "Más Recientes";
+            // 
+            // cmbBuscar
+            // 
+            this.cmbBuscar.FormattingEnabled = true;
+            this.cmbBuscar.Location = new System.Drawing.Point(60, 56);
+            this.cmbBuscar.Name = "cmbBuscar";
+            this.cmbBuscar.Size = new System.Drawing.Size(535, 28);
+            this.cmbBuscar.TabIndex = 1;
+            // 
+            // btnIr
+            // 
+            this.btnIr.Location = new System.Drawing.Point(633, 56);
+            this.btnIr.Name = "btnIr";
+            this.btnIr.Size = new System.Drawing.Size(68, 28);
+            this.btnIr.TabIndex = 2;
+            this.btnIr.Text = "IR";
+            this.btnIr.UseVisualStyleBackColor = true;
+            this.btnIr.Click += new System.EventHandler(this.btnIr_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(60, 105);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(1066, 496);
+            this.webBrowser1.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(739, 57);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 26);
+            this.textBox1.TabIndex = 4;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(1132, 164);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(240, 275);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(1382, 647);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.btnIr);
+            this.Controls.Add(this.cmbBuscar);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.Text = "Navegador Web";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem navegadorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem inicioToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem haciaAdelanteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem haciaAtrásToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem historialToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem másVisitadasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem másRecientesToolStripMenuItem;
+        private System.Windows.Forms.ComboBox cmbBuscar;
+        private System.Windows.Forms.Button btnIr;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace LaboratorioNo4
+{
+    public partial class Form1 : Form
+    {
+        int veces ;
+        List<url> datos = new List<url>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //cmbBuscar.SelectedIndex = 0;
+            webBrowser1.GoHome();
+            string nombreArchivo = @"C:\ArchivoLab3.txt";
+
+            FileStream stream = new FileStream(nombreArchivo, FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(stream);
+
+            while (reader.Peek() > -1)
+            {
+                string texto = reader.ReadLine();
+                cmbBuscar.Items.Add(texto);
+            }
+            reader.Close();
+        }
+
+        private void navegarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnIr_Click(object sender, EventArgs e)
+        {
+            String uri = cmbBuscar.Text;
+         
+            //Angely Esmeralda Thomas Cortéz -  202108047 - Ingeniería en Sistemas 
+            //Tercer Semestre Universidad Mesoamericana, Programación
+
+            //busca si el texto ya existe dentro de datos
+            int pos = datos.FindIndex(n => n.texto == uri);           
+            //si no existe lo creamos y lo agregamos a la lista
+            if (pos == -1)
+            {
+                veces = 0;
+                url dato = new url();      
+                veces++;
+                textBox1.Text = veces.ToString();
+                dato.texto = cmbBuscar.Text;              
+                //condicion para url
+                if (uri.Contains("."))
+                {
+                    if (uri.Contains("https://"))
+                    {
+                        //Buscar lo seleccionado en el combobox
+                        webBrowser1.Navigate(new Uri(cmbBuscar.Text));
+                    }
+                    else
+                    {
+                        webBrowser1.Navigate(new Uri("https://" + cmbBuscar.Text));
+                        cmbBuscar.Text = "https://" + cmbBuscar.Text;
+                    }
+                }
+                else
+                {
+                    uri = "http://www.google.com/search?q=" + uri;
+                    webBrowser1.Navigate(new Uri(uri));
+                }
+
+
+                datos.Add(dato);
+                cmbBuscar.Items.Add(uri);
+                Guardar(@"C:\ArchivoLab3.txt", uri);
+            }
+            else
+            {
+                veces = datos[pos].numero++;
+                textBox1.Text = veces.ToString();
+               
+                //condicion para url
+                if (uri.Contains("."))
+                {
+                    if (uri.Contains("https://"))
+                    {
+                        //Buscar lo seleccionado en el combobox
+                        webBrowser1.Navigate(new Uri(cmbBuscar.Text));
+                    }
+                    else
+                    {
+                        webBrowser1.Navigate(new Uri("https://" + cmbBuscar.Text));
+                        cmbBuscar.Text = "https://" + cmbBuscar.Text;
+                    }
+                }
+                else
+                {
+                    uri = "http://www.google.com/search?q=" + uri;
+                    webBrowser1.Navigate(new Uri(uri));
+                }
+
+            }
+
+        }
+
+        private void Guardar(string nombreArchivo, string texto)
+        {
+            FileStream stream = new FileStream(nombreArchivo, FileMode.Append, FileAccess.Write);
+
+            StreamWriter writer = new StreamWriter(stream);
+
+            writer.WriteLine(texto);
+
+            writer.Close();
+        }
+
+        private void inicioToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoHome();
+        }
+
+        private void haciaAdelanteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoForward();
+        }
+
+        private void haciaAtrásToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoBack();
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void másVisitadasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            foreach (url item in datos)
+            {
+                datos = datos.OrderBy(n => n.numero).ToList();
+                cmbBuscar.Items.Add(datos.ToList());
+                Guardar2A(@"C:\ArchivoLab3.txt", datos.ToString());
+                Mostrar();
+            }
+            
+        }
+
+
+        private void Guardar2A(string nombreArchivo, string datos)
+        {
+            FileStream stream = new FileStream(nombreArchivo, FileMode.OpenOrCreate, FileAccess.Write);
+
+            StreamWriter writer = new StreamWriter(stream);
+
+            writer.WriteLine(datos.ToString());
+
+            writer.Close();
+        }
+
+        private void Mostrar()
+        {
+            dataGridView1.DataSource = null;
+            dataGridView1.Refresh();
+
+            dataGridView1.DataSource = datos;
+            dataGridView1.Refresh();
+        }
+    }
+}
